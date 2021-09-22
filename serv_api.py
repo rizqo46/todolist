ae9a794b4f4417a912a92b8a888738e491490294
Abyan Rizqo <rizqo1412@gmail.com>
2021-09-22 08:19:56 +0700

    Make change on auth

--- serv_api.py
+++ serv_api.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
 import os
 
-# Init Flask
+# Init Flask for auth commit
 app = Flask(__name__)
 base_dir = os.path.abspath(os.path.dirname(__file__))
 
